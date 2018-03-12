b117eeea331b9f09e5661fdb6a1b47a2e4e07059
Jockero1991 <black_jack.1991@mail.ru>
2018-03-13 01:01:46 +0300

    Разработка тестового сценария
    Добавил инициализатор драйвера.
    Добавил и описал сценарий логина в админку.

---
+++ litecart-login/login.py
@@ -0,0 +1,30 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+
+@pytest.fixture
+def driver(request):
+    chrome_options = Options()
+    chrome_options.add_argument("--window-size=1920,1080")
+    wd = webdriver.Chrome(chrome_options=chrome_options)
+
+    print(wd.capabilities)
+    request.addfinalizer(wd.quit)
+    return wd
+
+def test_login_success(driver):
+    wait = WebDriverWait(driver, 4)
+    print('Открываем страницу админки')
+    driver.get('http://localhost/litecart/admin/')
+    sleep(2)
+    print('Вводим логин')
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@name="username"]'))).send_keys('admin')
+    print('Вводим пароль')
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@name="password"]'))).send_keys('admin')
+    print('Жмем кнопку login')
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@name="login"]'))).click()
+    sleep(2)
