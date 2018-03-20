87f4427a2111f33d6f5177a00612fe16118e9945
Jockero1991 <black_jack.1991@mail.ru>
2018-03-21 01:02:04 +0300

    добавил проверку стикеров для товаров
    файл с тестом ducks_n_stickers.py
    - проверка наличия стикеров
    - проверка что стикеры одинаковые у одинаковых товаров
    - обработка ошибок

--- litecart-login/ducks_n_stickers.py
+++ litecart-login/ducks_n_stickers.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
+#import assert
 
 
 @pytest.fixture
@@ -18,5 +19,24 @@ def driver(request):
 
 def test_ducks(driver):
     driver.get('http://localhost/litecart/en/')
-    all_ducks = driver.find_elements_by_xpath('//*[@class="product column shadow hover-light"]/a')
-    print(all_ducks[1].get_attribute('title')) # [0].get_attribute('alt')
+    try:
+        all_ducks = driver.find_elements_by_xpath('//*[@class="product column shadow hover-light"]/a[1]/div[1]/img')
+        all_stickers = driver.find_elements_by_xpath('//*[@class="product column shadow hover-light"]/a[1]/div[1]/div[1]')
+    except:
+        print('Ошибка при получении списков уток и стикеров')
+    pairs = {}
+    err_count = 0
+    for x in range(len(all_ducks)):
+        if all_ducks[x].get_attribute('alt') not in pairs.keys():
+            if all_stickers[x].get_attribute('class') != None:
+                pairs.update({all_ducks[x].get_attribute('alt') : all_stickers[x].get_attribute('class')})
+            else:
+                print('у утки ' + str(all_ducks[x].get_attribute('alt')) + ' нет стикера.')
+        else:
+            if pairs[all_ducks[x].get_attribute('alt')] != all_stickers[x].get_attribute('class'):
+                err_count += 1
+                print(all_ducks[x].get_attribute('alt'), str('already exists: ' + pairs[all_ducks[x].get_attribute('alt')]), str('trying to add: ' + all_stickers[x].get_attribute('class')))
+    if err_count==0:
+        print('У всех уток уникальные стикеры')
+    else:
+        print('Кол-во разных стикеров для одинаковых уток: ' + str(err_count))
