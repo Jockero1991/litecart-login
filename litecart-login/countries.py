2facb0bb60ef29f513c91f9205d3bc95235f2655
Jockero1991 <black_jack.1991@mail.ru>
2018-03-23 08:42:26 +0300

    сортировка первого экрана готова
    но надо еще сделать выборку списка стран на других экранах.

--- litecart-login/countries.py
+++ litecart-login/countries.py
@@ -32,14 +32,31 @@ def test_login_success(driver):
 
 def sorting_countr(driver):
     driver.get('http://localhost/litecart/admin/?app=countries&doc=countries')
-    table_text = driver.find_elements_by_xpath('//*[@class="dataTable"]//tr[@class="row"]')
-    table_text = [x.text for x in table_text] # долго отрабатывает, причем неважно какой цикл ни ставь.
+    table = driver.find_elements_by_xpath('//*[@class="dataTable"]//tr[@class="row"]')
+    table_text = [x.text for x in table] # долго отрабатывает, причем неважно какой цикл ни ставь.
     countrys = []# Массив стран
     count_geo = [] # массив кол-ва геозон
+    ordered_list = []# Массив стран отсортированный
+    links = [] # ссылки на стран с кол-вом геозон больше 0
     for x in range(len(table_text)):
         countrys.append(table_text[x].split(' ')[2 : (len(table_text[x].split(' ')) - 1)])
         countrys[x] = ' '.join(countrys[x])
+        ordered_list = sorted(countrys)
         count_geo.append(table_text[x].split(' ')[(len(table_text[x].split(' ')) - 1)])
-        print(countrys[x], count_geo[x])
-
-    
+        if int(count_geo[x]) > 0:
+            links.append(table[x].find_element_by_xpath('//*[@class="dataTable"]//tr[@class="row"]//a').get_attribute('href'))
+        #print(countrys[x], count_geo[x])
+    ordered_list = sorted(countrys)
+    #print(links[0].get_attribute('href'))
+    check_sum = 0
+    for y in range(len(countrys)):
+        if countrys[y] == ordered_list[y]:
+            check_sum += 1
+    if check_sum == len(ordered_list):
+        print('Список стран отсортирован по алфавиту')
+    for z in range(len(links)):
+        list_for_chk=[]
+        #print(links[z].get_attribute('href'))
+        driver.get(links[z])
+        list_for_chk=driver.find_elements_by_xpath('//*[@id="table-zones"]/tbody//input[@name="name"]')
+        print(list_for_chk[0].get_attribute('value'))
