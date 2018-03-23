0fa367fecdeb0797c2b94a0757adfb9aa50410b3
Vladislav Kovalenko <black_jack.1991@mail.ru>
2018-03-23 18:57:15 +0300

    Дописал проверку сортировку на странице редактирования страны

--- litecart-login/countries.py
+++ litecart-login/countries.py
@@ -27,27 +27,28 @@ def test_login_success(driver):
     wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@name="password"]'))).send_keys('admin')
     print('Жмем кнопку login')
     wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@name="login"]'))).click()
-    sleep(2)
+    sleep(1)
     sorting_countr(driver)
 
 def sorting_countr(driver):
     driver.get('http://localhost/litecart/admin/?app=countries&doc=countries')
     table = driver.find_elements_by_xpath('//*[@class="dataTable"]//tr[@class="row"]')
+    print(table[37].text)
     table_text = [x.text for x in table] # долго отрабатывает, причем неважно какой цикл ни ставь.
     countrys = []# Массив стран
     count_geo = [] # массив кол-ва геозон
     ordered_list = []# Массив стран отсортированный
-    links = [] # ссылки на стран с кол-вом геозон больше 0
+    links = [] # ссылки на страны с кол-вом геозон больше 0
     for x in range(len(table_text)):
         countrys.append(table_text[x].split(' ')[2 : (len(table_text[x].split(' ')) - 1)])
         countrys[x] = ' '.join(countrys[x])
         ordered_list = sorted(countrys)
-        count_geo.append(table_text[x].split(' ')[(len(table_text[x].split(' ')) - 1)])
-        if int(count_geo[x]) > 0:
-            links.append(table[x].find_element_by_xpath('//*[@class="dataTable"]//tr[@class="row"]//a').get_attribute('href'))
+        count_geo.append(int(table_text[x].split(' ')[(len(table_text[x].split(' ')) - 1)]))
+        if count_geo[x] > 0:
+            links.append(table[x].find_element_by_xpath('//*[@class="dataTable"]//tr[@class="row"][' + str(x+1) + ']//a').get_attribute('href'))
         #print(countrys[x], count_geo[x])
     ordered_list = sorted(countrys)
-    #print(links[0].get_attribute('href'))
+    print(links)
     check_sum = 0
     for y in range(len(countrys)):
         if countrys[y] == ordered_list[y]:
@@ -56,7 +57,22 @@ def sorting_countr(driver):
         print('Список стран отсортирован по алфавиту')
     for z in range(len(links)):
         list_for_chk=[]
+        check_sum = 0
         #print(links[z].get_attribute('href'))
         driver.get(links[z])
-        list_for_chk=driver.find_elements_by_xpath('//*[@id="table-zones"]/tbody//input[@name="name"]')
-        print(list_for_chk[0].get_attribute('value'))
+        sleep(6)
+        list_for_chk=driver.find_elements_by_xpath('//*[@id="table-zones"]/tbody/tr//td[3]')
+        internal_ordered_list = []# список стран внутри страницы
+        errors = []
+        list_for_chk = [x.text for x in range(len(list_for_chk))]
+        ord_list = sorted(list_for_chk)
+        for i in range(len(list_for_chk)):
+            if list_for_chk[i] == ord_list[i]:
+                check_sum += 1
+            else:
+                errors.append(list_for_chk[i])
+        if check_sum==len(list_for_chk):
+            print('Список временных зон страны отсортирован')
+        else:
+            print('Список не отсортирован!')
+        #print(list_for_chk[i].text) #[0].get_attribute('value')
