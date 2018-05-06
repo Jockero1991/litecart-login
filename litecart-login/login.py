2d0001784e96ce230b56ecaf479acc6b0ef72287
Jockero1991 <black_jack.1991@mail.ru>
2018-05-06 10:28:01 +0300

    добавил логирование в тесте
    кроме того добавил функцию для получения скриншота при ошибке

--- litecart-login/login.py
+++ litecart-login/login.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -5,19 +6,33 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
+from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
+
+class MyListener(AbstractEventListener):
+    def before_find(self, by, value, driver):
+        print(by, value)
+    def after_find(self, by, value, driver):
+        print(by, value, "found")
+    def on_exception(self, exception, driver):
+        print(exception)
+        tod=str(dt.datetime.today().timestamp())
+        tod = f"Screen-{tod}.png"
+        #print(tod)
+        driver.get_screenshot_as_file(tod)
+        
 
 @pytest.fixture
 def driver(request):
     chrome_options = Options()
     chrome_options.add_argument("--window-size=1920,1080")
-    wd = webdriver.Chrome(chrome_options=chrome_options)
+    wd = EventFiringWebDriver(webdriver.Chrome(chrome_options=chrome_options), MyListener())
 
     print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
 
 def test_login_success(driver):
-    wait = WebDriverWait(driver, 4)
+    wait = WebDriverWait(driver, 1)
     print('Открываем страницу админки')
     driver.get('http://localhost/litecart/admin/')
     sleep(2)
